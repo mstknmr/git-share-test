d8e807a1f7b96e21866505b1af723c011d77f7e3
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-06-18 13:46:05 +0900

    Carクラス実装

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Car {
+	private int hayasa;
+	private int iro;
+	public int getHayasa() {
+		return hayasa;
+	}
+	public void setHayasa(int hayasa) {
+		this.hayasa = hayasa;
+	}
+	public int getIro() {
+		return iro;
+	}
+	public void setIro(int iro) {
+		this.iro = iro;
+	}
+	
+
+}
